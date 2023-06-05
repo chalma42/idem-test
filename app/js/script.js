39bfa79bbabad9fa681f6b9635328b03e358fc86
Alexandr <chalma123@yandex.ru>
2023-06-05 23:48:08 +0400

    карта
    переключение элементов на карте

--- app/js/script.js
+++ app/js/script.js
@@ -7,4 +7,22 @@ document.addEventListener('DOMContentLoaded', function(){
             prevEl: ".swiper-button-prev",
         },
     });
+
+    const tabs = document.querySelectorAll('.map__tabs-item');
+    let pins = document.querySelectorAll('.map__pin');
+    tabs.forEach(element => {
+        element.addEventListener("click", function(){
+            tabs.forEach(el => {
+                el.classList.remove('current')
+            });
+            this.classList.add('current');
+            pins.forEach(elem => {
+                if(elem.classList.contains(this.dataset.value)) {
+                    elem.classList.add('active');
+                } else {
+                    elem.classList.remove('active');
+                }
+            });
+        });
+    });
 })
